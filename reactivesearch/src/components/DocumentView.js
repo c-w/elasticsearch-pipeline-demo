c0996066e631a38d2fce9ef8b20a066d62dabecb
Clemens Wolff <clewolff@microsoft.com>
2019-07-10 14:23:37 -0400

    Render text analytics results

--- reactivesearch/src/components/DocumentView.js
+++ reactivesearch/src/components/DocumentView.js
@@ -1,24 +1,38 @@
 import React from "react";
 
-export default function DocumentView(props) {
-  const opennlp = props.selectedDocument.opennlp
-    ? props.selectedDocument.opennlp
-    : {};
+function EntityList(props) {
+  const entities = Object.entries(props.entities);
+
+  if (!entities.length) {
+    return null;
+  }
 
   return (
-    <div className="col col-12 px3">
-      <h3>Document Content</h3>
-      <p>{props.selectedDocument ? props.selectedDocument.doc.content : ""}</p>
-      <h3>OpenNLP Results</h3>
+    <div>
+      <h3>{props.header}</h3>
       <ul>
-        {Object.entries(opennlp).map(([entityType, entityList]) =>
-          entityList.map((entity, index) =>
+        {entities.map(([entityType, entityList]) =>
+          (Array.isArray(entityList) ? entityList : [entityList]).map((entity, index) =>
             <li key={index}>
               <b>{entityType}</b>: {entity}
             </li>
           )
         )}
       </ul>
+    </div>
+  );
+}
+
+export default function DocumentView(props) {
+  const opennlp = props.selectedDocument.opennlp || {};
+  const textanalytics = props.selectedDocument.textanalytics || {};
+
+  return (
+    <div className="col col-12 px3">
+      <h3>Document Content</h3>
+      <p>{props.selectedDocument ? props.selectedDocument.doc.content : ""}</p>
+      <EntityList header="OpenNLP Results" entities={opennlp} />
+      <EntityList header="Text Analytics Results" entities={textanalytics} />
     </div>
   );
 }
