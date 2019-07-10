684a18167923035f75fb26bb52ba2ab5efa12a41
Clemens Wolff <clewolff@microsoft.com>
2019-07-10 14:28:21 -0400

    Avoid always re-creating onclick function

--- reactivesearch/src/components/SearchResultList.js
+++ reactivesearch/src/components/SearchResultList.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-export default function SearchResultList(props) {
-  const response = props.response;
-  return (
-    <div>
-      <p
-        className="border-top mb3"
-        onClick={() => props.onSelectDocument(response)}
-      >
-        <b>file</b>: {response.azurestorage.blob}
-      </p>
-    </div>
-  );
+export default class SearchResultList extends React.Component {
+  onClick = () => this.props.onSelectDocument(this.props.response);
+
+  render() {
+    return (
+      <div>
+        <p className="border-top mb3" onClick={this.onClick}>
+          <b>file</b>: {this.props.response.azurestorage.blob}
+        </p>
+      </div>
+    );
+  }
 }
