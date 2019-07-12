ad58528edefa01e2733188017ccba0bf506b1cb0
Michael Perel <michaelsethperel@gmail.com>
2019-07-12 11:15:47 -0500

    replaced shell with openshift rest client

--- cleanup/login.sh
+++ cleanup/login.sh
@@ -10,9 +10,3 @@ oc login "$OC_MASTER_SERVER_DNS" \
     --username="$OC_USERNAME" \
     --password="$OC_PASSWORD" \
     --insecure-skip-tls-verify=true
-
-
-# DELETE PROJECT FROM CLUSTER
-oc project default
-oc_project_name=$(cat project_to_delete.txt)
-oc delete project "$oc_project_name"
--- cleanup/server.py
+++ cleanup/server.py
@@ -1,8 +1,20 @@
-import os
-import subprocess
+import sys
 
 import hug
+from kubernetes import client, config
+from openshift.dynamic import DynamicClient
+from openshift.dynamic.exceptions import NotFoundError
 
+
+def _delete_project(project_name):
+    k8s_client = config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+    projects = dyn_client.resources.get(api_version='project.openshift.io/v1', kind='Project')
+    try:
+        print(f'attempting to delete {project_name}...')
+        print(projects.delete(name=project_name))
+    except NotFoundError:
+        print(f'{project_name} was not found.')
 
 @hug.post('/')
 def cleanup_project(body):
@@ -12,13 +24,5 @@ def cleanup_project(body):
         if pr_info:
             pr_id = pr_info.get('number')
             if pr_id is not None:
-                with open('project_to_delete.txt', 'w', encoding='utf-8') as f:
-                    project_name = f'ci-refs-pull-{pr_id}-merge'
-                    f.write(project_name)
-                try:
-                    subprocess.check_output(['/bin/bash', '-c', './cleanup.sh'])
-                    print('cleaned up:', project_name, flush=True)
-                except subprocess.CalledProcessError as e:
-                    print('cleanup failed:', e, flush=True)
-                finally:
-                    os.remove('project_to_delete.txt')
+                _delete_project(f'ci-refs-pull-{pr_id}-merge')
+    sys.stdout.flush()
